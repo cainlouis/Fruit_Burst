9133b23ced4d675031b486bc1ba65fbf69a0135f
Nael Louis <nael.louis1002@gmail.com>
2021-10-16 13:45:18 -0400

    Change the way the grid is populated'

--- FruitBurstBackend/FruitGrid.cs
+++ FruitBurstBackend/FruitGrid.cs
@@ -5,7 +5,7 @@ namespace FruitBurstBackend
     public class FruitGrid
     {
         private IFruit[,] grid;
-        public int Length{get;}
+        public int Height{get;}
         public int Width {get;}
 
         public IFruit this[int i, int j]
@@ -13,33 +13,25 @@ namespace FruitBurstBackend
             get {return grid[i,j];}
             set {this.grid[i,j] = value;}
         }
-        public FruitGrid(int length, int width) 
+
+        //parameterized constructor, takes the height and width for the grid then call populateGrid to fill it
+        public FruitGrid(int height, int width) 
         {   
-            Length = length;
+            Height = height;
             Width = width;
-            grid = new IFruit[length, width];
+            grid = new IFruit[height, width];
             PopulateGrid();
         }
 
+        //Populate the grid with fruit
         private void PopulateGrid() 
-        {
-            int numMagic;
-            int numExplosive;
-            IFruit ordinaryFruit = new Fruit();
-            FruitDecorator explosive = new ExplodingFruit(ordinaryFruit);
-            FruitDecorator magic = new MagicFruit(ordinaryFruit);
-            if (Length < 20) {
-                numMagic = 3;
-                numExplosive = 1;
-            }
-            else
-            {
-                numMagic = 5;
-                numExplosive = 3;
-            }
-            insertDecoratedFruit(numMagic, magic);
-            insertDecoratedFruit(numExplosive, explosive);
-            for (int i = 0; i < Length; i++)
+        {   
+            //insert the decorated fruits first
+            InsertDecoratedFruit();
+
+            IFruit ordinaryFruit = new Fruit();   
+            //For all the place left in grid place normal fruit
+            for (int i = 0; i < Height; i++)
             {
                 for (int j = 0; j < Width; j++) 
                 {
@@ -49,25 +41,62 @@ namespace FruitBurstBackend
                 }
             }
         }
-
+        
+        //Return the fruit at [i,j]'s visibility 
         public bool GetVisibility(int i, int j)
         {
             return grid[i,j].IsVisible;
         }
 
-        private void insertDecoratedFruit(int num, FruitDecorator fruit) {
+        //Insert the decorated fruits at random positions in the grid | doing so exploding fruit has different points
+        private void InsertDecoratedFruit() {
+            int num;
+            int counter = 0;
             Random random = new Random();
             int rand1;
             int rand2;
-            for (int i = 1; i <= num; i++) {
-                rand1 = random.Next(Width);
-                rand2 = random.Next(Length);
-                if (grid[rand1, rand2] != null) {
-                    i--;
-                } else{
-                    grid[rand1, rand2] = fruit;
+            do
+            {
+                //at counter = 0 chose the number of  magic fruit I want according to the height and width
+                if (counter == 0)
+                {
+                    if (Height < 20 && Width < 20)
+                    {
+                        num = 3;
+                    } else {
+                        num = 5;
+                    }
+                } else {
+                    //If counter is not  zero (can only be 1 here) chose the number of exploding fruits
+                    if (Height < 20 && Width < 20)
+                    {
+                        num = 1;
+                    } else {
+                        num = 3;
+                    }
                 }
-            }
+                //for the number chosen
+                for (int i = 1; i <= num; i++)
+                {
+                    //get random number for the position
+                    rand1 = random.Next(Width);
+                    rand2 = random.Next(Height);
+                    //if that position isn't null decrement i so it randomly choose another position 
+                    if (grid[rand1, rand2] != null)
+                    {
+                        i--;
+                    } else {
+                        //using the counter place the decorated fruit
+                        if (counter == 0)
+                        {
+                            grid[rand1, rand2] = new MagicFruit(new Fruit());
+                        } else {
+                            grid[rand1, rand2] = new ExplodingFruit(new Fruit());
+                        }
+                    }
+                }
+                counter++;
+            } while (counter < 2);
         }
     }
 }
