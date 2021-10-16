f3f5721ef943fa0b9c7ebc30376e4afa99d324d7
Nael Louis <nael.louis1002@gmail.com>
2021-10-16 12:57:22 -0400

    Really Finish MagicFruitTests

--- FruitBurstUnitTests/MagicFruitTests.cs
+++ FruitBurstUnitTests/MagicFruitTests.cs
@@ -10,15 +10,42 @@ namespace FruitBurstUnitTests
         public void TestInitialization_HPEquals20()
         {
             MagicFruit mFruit = new MagicFruit(new Fruit());
-            int expected = 20;
+            int mected = 20;
             int result = mFruit.HP;
-            Assert.AreEqual(expected,  result);
+            Assert.AreEqual(mected,  result);
         }
 
         [TestMethod]
         public void TestInitialization_IsVisibleIsFalse()
         {
             MagicFruit mFruit = new MagicFruit(new Fruit());
+            Assert.IsFalse(mFruit.IsVisible);
+        }
+
+        [TestMethod]
+        public void TestMakeVisible_IsVisibleIsTrue()
+        {
+            MagicFruit mFruit = new MagicFruit(new Fruit());
+            mFruit.MakeVisible();
+            Assert.IsTrue(mFruit.IsVisible);
+        }
+
+        [TestMethod]
+        public void TestMakeInvisible_IsVisibleIsTrueWithHPEquals20()
+        {
+            MagicFruit mFruit = new MagicFruit(new Fruit());
+            mFruit.MakeVisible();
+            mFruit.MakeInvisible();
+            Assert.IsTrue(mFruit.IsVisible);
+        }
+
+        [TestMethod]
+        public void TestMakeInvisible_IsVisibleIsTrueWithHPEquals0()
+        {
+            MagicFruit mFruit = new MagicFruit(new Fruit());
+            mFruit.MakeVisible();
+            mFruit.HP = 0;
+            mFruit.MakeInvisible();
             Assert.IsFalse(mFruit.IsVisible);
         }
     }
